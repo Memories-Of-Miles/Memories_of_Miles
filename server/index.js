c0ba2a822b5b08c09ae203966702c5fe4b438bf3
Nitish pandey <Nitish7040@users.noreply.github.com>
2026-01-17 02:50:38 +0530

    initial changes

--- server/index.js
+++ server/index.js
@@ -85,5 +85,5 @@ app.use((err, req, res, next) => {
 
 /* -------------------- SERVER START -------------------- */
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(` Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
